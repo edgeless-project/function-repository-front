76ceba0d5a74254c4fd6b88da594e92e0444de14
Francisco Vicente <franvipa@gmail.com>
2024-03-12 13:19:49 +0100

    Fix typo in a comment

--- src/pages/workflow/edit/[name].tsx
+++ src/pages/workflow/edit/[name].tsx
@@ -64,7 +64,7 @@ export default function WorkflowEdit() {
     setIsSaving(true);
     setModalOpen(true);
 
-    // Create the workflow in the API
+    // Update the workflow in the API
     try {
       const workflowData: ApiRequestUpdateWorkflow = {
         ...workflowJSON as ApiRequestUpdateWorkflow
