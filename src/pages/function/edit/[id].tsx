dc8df3ceb88b1841e2a83e9d56eb35bef8a118dd
Marc Espinosa i Tejada(W135175) <marc.espinosa@worldline.com>
2024-03-20 12:59:37 +0100

    Display form in card and Title

--- src/pages/function/edit/[id].tsx
+++ src/pages/function/edit/[id].tsx
@@ -18,29 +18,11 @@ import DialogSave from "@/components/utils/DialogSave";
 
 //Form Schema verification
 const formSchema = z.object({
-    version: z.string().min(1, 'The version must contain at least 1 character'),
+    version: z.string().min(1, 'The version must contain at least 1 character'),//TODO: delete attr
     functionType: z.enum(['RUST_WASM']),
     file: z.instanceof(File).optional(),
     outputs: z.string()
 })
-    .refine(
-        (data) => {
-            return true;
-        },
-        {
-            message: "Version must be newer than the las one saved",
-            path: ["version"],
-        }
-    )
-    .refine(
-        (data) => {
-            return !hasMiddleSpaces(data.version);
-        },
-        {
-            message: "The version must not contain spaces",
-            path: ["version"],
-        }
-    )
     .refine(
         (data) => {
             return !(!data.file || data.file.name === "");
@@ -68,7 +50,7 @@ const formSchema = z.object({
 
 export default function FunctionEdit() {
   const router = useRouter();
-  const id = router.query.id;
+  const id = router.query.id as string;
 
   const [functions, setFunctions] = useState<FunctionComplete[]>([]);
   const [loading, setLoading] = useState(true);
@@ -85,7 +67,7 @@ export default function FunctionEdit() {
           setFunctions(functions);
           setLoading(false);
         })
-        .catch(error => console.error(error));
+        .catch(error => console.error(error)); //TODO: Error threw
   }, []);
 
   //Form Data
@@ -116,11 +98,10 @@ export default function FunctionEdit() {
           return;
       }
 
-      // Create the function in the API
+      // Create the function with an API call
       try {
           const outputs = splitOutputs(data.outputs);
-          if (id) //TODO: Throw exception if no id and create update function
-              await updateFunction(id.toString(), codeId, data.functionType, data.version.trim(), outputs);
+          await updateFunction(id, codeId, data.functionType, data.version.trim(), outputs);
 
           setSaveMessage('The function has been updated successfully');
           setResultOk(true);
@@ -156,101 +137,104 @@ export default function FunctionEdit() {
             </CardContent>
         </Card>
 
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(handleSubmit)}>
-                <CardContent className="max-w-5xl">
-
-                    <FormField
-                        control={form.control}
-                        name="version"
-                        render={({field}) => {
-                            return (
-                                <FormItem className="mt-5">
-                                    <FormLabel>Version</FormLabel>
-                                    <FormControl>
-                                        <Input type="text" placeholder="version" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            );
-                        }}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="functionType"
-                        render={({field}) => {
-                            return (
-                                <FormItem className="mt-5">
-                                    <FormLabel>Function type</FormLabel>
-                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+        <Card>
+            <CardHeader>
+                <CardTitle>Function class specification</CardTitle>
+            </CardHeader>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(handleSubmit)}>
+                    <CardContent className="max-w-5xl">
+                        <FormField
+                            control={form.control}
+                            name="version"
+                            render={({field}) => {
+                                return (
+                                    <FormItem className="mt-5">
+                                        <FormLabel>Version</FormLabel>
                                         <FormControl>
-                                            <SelectTrigger>
-                                                <SelectValue placeholder="Select a type" />
-                                            </SelectTrigger>
+                                            <Input type="text" placeholder="version" {...field} />
                                         </FormControl>
-                                        <SelectContent>
-                                            <SelectItem value="RUST_WASM">RUST_WASM</SelectItem>
-                                        </SelectContent>
-                                    </Select>
-                                    <FormMessage />
-                                </FormItem>
-                            );
-                        }}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="file"
-                        render={({field}) => {
-                            return (
-                                <FormItem className="mt-5">
-                                    <FormLabel>Code file</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            type="file"
-                                            onChange={(e) =>
-                                                field.onChange(e.target.files ? e.target.files[0] : null)
-                                            }
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            );
-                        }}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="outputs"
-                        render={({field}) => {
-                            return (
-                                <FormItem className="mt-5">
-                                    <FormLabel>Outputs</FormLabel>
-                                    <FormControl>
-                                        <Input type="text" placeholder="Outputs" {...field} />
-                                    </FormControl>
-                                    <FormDescription>Here you can define the outputs, separated by commas.</FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            );
-                        }}
-                    />
-                </CardContent>
-                <CardFooter className="flex justify-between max-w-5xl mt-8">
-                    <Button
-                        variant="outline"
-                        onClick={() => { router.back() }}
-                    >Cancel</Button>
-                    <Button type="submit">Save</Button>
-                </CardFooter>
-            </form>
-        </Form>
-
-        <DialogSave
-            isOpen={modalOpen}
-            title="Saving function"
-            description={saveMessage}
-            isLoading={isSaving}
-            onClose={closeModal}
-        />
+                                        <FormMessage />
+                                    </FormItem>
+                                );
+                            }}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="functionType"
+                            render={({field}) => {
+                                return (
+                                    <FormItem className="mt-5">
+                                        <FormLabel>Function type</FormLabel>
+                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue placeholder="Select a type" />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectItem value="RUST_WASM">RUST_WASM</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                        <FormMessage />
+                                    </FormItem>
+                                );
+                            }}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="file"
+                            render={({field}) => {
+                                return (
+                                    <FormItem className="mt-5">
+                                        <FormLabel>Code file</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                type="file"
+                                                onChange={(e) =>
+                                                    field.onChange(e.target.files ? e.target.files[0] : null)
+                                                }
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                );
+                            }}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="outputs"
+                            render={({field}) => {
+                                return (
+                                    <FormItem className="mt-5">
+                                        <FormLabel>Outputs</FormLabel>
+                                        <FormControl>
+                                            <Input type="text" placeholder="Outputs" {...field} />
+                                        </FormControl>
+                                        <FormDescription>Here you can define the outputs, separated by commas.</FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                );
+                            }}
+                        />
+                    </CardContent>
+                    <CardFooter className="flex justify-between max-w-5xl mt-8">
+                        <Button
+                            variant="outline"
+                            onClick={() => { router.back() }}
+                        >Cancel</Button>
+                        <Button type="submit">Save</Button>
+                    </CardFooter>
+                </form>
+            </Form>
+            <DialogSave
+                isOpen={modalOpen}
+                title="Saving function"
+                description={saveMessage}
+                isLoading={isSaving}
+                onClose={closeModal}
+            />
+        </Card>
         </div>}
     </Layout>
   );
