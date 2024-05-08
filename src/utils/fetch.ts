0970c1ab548382cf8eaa7209781652402f488274
Francisco Vicente <franvipa@gmail.com>
2024-05-08 12:49:05 +0200

    Add logic to work with the basic auth

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,10 +1,14 @@
+const serverBasicAuthEncoded = process.env.NEXT_PUBLIC_SERVER_BASIC_AUTH_ENCODED ? process.env.NEXT_PUBLIC_SERVER_BASIC_AUTH_ENCODED : '';
+
 export const buildFetchHeaders = (accessToken?: string):HeadersInit  => {
   const headers: HeadersInit = {
     'Content-Type': 'application/json',
     'accept': 'application/json'
   };
-  if(accessToken) {
+  if (accessToken) {
     headers['Authorization'] = `bearer ${accessToken}`;
+  } else if (serverBasicAuthEncoded) {
+    headers['Authorization'] = `Basic ${serverBasicAuthEncoded}`;
   }
   return headers;
 };
@@ -14,8 +18,10 @@ export const buildFileFetchHeaders = (accessToken?: string):HeadersInit  => {
     //'Content-Type': 'multipart/form-data',
     'accept': 'application/json'
   };
-  if(accessToken) {
+  if (accessToken) {
     headers['Authorization'] = `bearer ${accessToken}`;
+  } else if (serverBasicAuthEncoded) {
+    headers['Authorization'] = `Basic ${serverBasicAuthEncoded}`;
   }
   return headers;
 };
